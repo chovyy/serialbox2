370faa6a5e9b0646c111465651e5c1f4eb825a7e
Christian Hovy <hovy@informatik.uni-hamburg.de>
2018-03-21 22:16:14 +0100

    [ftg] Cleaned up merge mess

--- src/serialbox-fortran/m_ser_ftg.f90
+++ src/serialbox-fortran/m_ser_ftg.f90
@@ -535,10 +535,7 @@ SUBROUTINE ftg_write_logical_0d(fieldname, field)
   IF (.NOT. bullshit) THEN
     CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field)
     serializer_has_written = .TRUE.
-<<<<<<< HEAD
     CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:loc', TRIM(ADJUSTL(ftg_loc_hex(C_LOC(field)))))
   END IF
 
@@ -565,10 +562,7 @@ SUBROUTINE ftg_write_logical_1d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'bool', fs_boolsize(), lbounds, ubounds)
     END IF
@@ -600,10 +594,7 @@ SUBROUTINE ftg_write_logical_2d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'bool', fs_boolsize(), lbounds, ubounds)
     END IF
@@ -637,10 +628,7 @@ SUBROUTINE ftg_write_logical_3d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'bool', fs_boolsize(), lbounds, ubounds)
     END IF
@@ -674,10 +662,7 @@ SUBROUTINE ftg_write_logical_4d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'bool', fs_boolsize(), lbounds, ubounds)
     END IF
@@ -705,10 +690,7 @@ SUBROUTINE ftg_write_bool_0d(fieldname, field)
   IF (.NOT. bullshit) THEN
     CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field)
     serializer_has_written = .TRUE.
-<<<<<<< HEAD
     CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:loc', TRIM(ADJUSTL(ftg_loc_hex(C_LOC(field)))))
   END IF
 
@@ -735,10 +717,7 @@ SUBROUTINE ftg_write_bool_1d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'bool', fs_boolsize(), lbounds, ubounds)
     END IF
@@ -770,10 +749,7 @@ SUBROUTINE ftg_write_bool_2d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'bool', fs_boolsize(), lbounds, ubounds)
     END IF
@@ -806,10 +782,7 @@ SUBROUTINE ftg_write_bool_3d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'bool', fs_boolsize(), lbounds, ubounds)
     END IF
@@ -843,10 +816,7 @@ SUBROUTINE ftg_write_bool_4d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'bool', fs_boolsize(), lbounds, ubounds)
     END IF
@@ -874,10 +844,7 @@ SUBROUTINE ftg_write_int_0d(fieldname, field)
   IF (.NOT. bullshit) THEN
     CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field)
     serializer_has_written = .TRUE.
-<<<<<<< HEAD
     CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:loc', TRIM(ADJUSTL(ftg_loc_hex(C_LOC(field)))))
   END IF
 
@@ -904,10 +871,7 @@ SUBROUTINE ftg_write_int_1d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'int', fs_intsize(), lbounds, ubounds)
     END IF
@@ -939,10 +903,7 @@ SUBROUTINE ftg_write_int_2d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'int', fs_intsize(), lbounds, ubounds)
     END IF
@@ -975,10 +936,7 @@ SUBROUTINE ftg_write_int_3d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'int', fs_intsize(), lbounds, ubounds)
     END IF
@@ -1012,10 +970,7 @@ SUBROUTINE ftg_write_int_4d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'int', fs_intsize(), lbounds, ubounds)
     END IF
@@ -1043,10 +998,7 @@ SUBROUTINE ftg_write_long_0d(fieldname, field)
   IF (.NOT. bullshit) THEN
     CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field)
     serializer_has_written = .TRUE.
-<<<<<<< HEAD
     CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:loc', TRIM(ADJUSTL(ftg_loc_hex(C_LOC(field)))))
   END IF
 
@@ -1073,10 +1025,7 @@ SUBROUTINE ftg_write_long_1d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'long', fs_longsize(), lbounds, ubounds)
     END IF
@@ -1108,10 +1057,7 @@ SUBROUTINE ftg_write_long_2d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'long', fs_longsize(), lbounds, ubounds)
     END IF
@@ -1144,10 +1090,7 @@ SUBROUTINE ftg_write_long_3d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'long', fs_longsize(), lbounds, ubounds)
     END IF
@@ -1181,10 +1124,7 @@ SUBROUTINE ftg_write_long_4d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'long', fs_longsize(), lbounds, ubounds)
     END IF
@@ -1239,10 +1179,7 @@ SUBROUTINE ftg_write_float_1d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'float', fs_floatsize(), lbounds, ubounds)
     END IF
@@ -1274,10 +1211,7 @@ SUBROUTINE ftg_write_float_2d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'float', fs_floatsize(), lbounds, ubounds)
     END IF
@@ -1310,10 +1244,7 @@ SUBROUTINE ftg_write_float_3d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'float', fs_floatsize(), lbounds, ubounds)
     END IF
@@ -1347,10 +1278,7 @@ SUBROUTINE ftg_write_float_4d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'float', fs_floatsize(), lbounds, ubounds)
     END IF
@@ -1378,10 +1306,7 @@ SUBROUTINE ftg_write_double_0d(fieldname, field)
   IF (.NOT. bullshit) THEN
     CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field)
     serializer_has_written = .TRUE.
-<<<<<<< HEAD
     CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:loc', TRIM(ADJUSTL(ftg_loc_hex(C_LOC(field)))))
   END IF
 
@@ -1408,10 +1333,7 @@ SUBROUTINE ftg_write_double_1d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'double', fs_doublesize(), lbounds, ubounds)
     END IF
@@ -1443,10 +1365,7 @@ SUBROUTINE ftg_write_double_2d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'double', fs_doublesize(), lbounds, ubounds)
     END IF
@@ -1479,10 +1398,7 @@ SUBROUTINE ftg_write_double_3d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'double', fs_doublesize(), lbounds, ubounds)
     END IF
@@ -1516,10 +1432,7 @@ SUBROUTINE ftg_write_double_4d(fieldname, field, lbounds, ubounds)
     IF (SIZE(field) > 0) THEN
       CALL fs_write_field(ftg_get_serializer(), ftg_get_savepoint(), fieldname, field, lbounds, ubounds)
       serializer_has_written = .TRUE.
-<<<<<<< HEAD
       CALL fs_add_field_metainfo(serializer, TRIM(fieldname), 'ftg:registered_only', .FALSE.)
-=======
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
     ELSE
       CALL ftg_register_only_internal(fieldname, 'double', fs_doublesize(), lbounds, ubounds)
     END IF
@@ -1630,16 +1543,11 @@ SUBROUTINE ftg_read_bool_2d(fieldname, field)
   LOGICAL(KIND=C_BOOL), INTENT(OUT), TARGET :: field(:,:)
   LOGICAL :: registered_only
 
-<<<<<<< HEAD
   IF (.NOT. ignore_not_existing .OR. ftg_field_exists(fieldname)) THEN
     CALL fs_get_field_metainfo(serializer, fieldname, 'ftg:registered_only', registered_only)
     IF (.NOT. registered_only) THEN
       CALL fs_read_field(serializer, savepoint, fieldname, field)
     END IF
-=======
-  IF (.NOT. ignore_not_existing .OR. (ftg_field_exists(fieldname) .AND. fs_get_total_size(serializer, fieldname) > 0)) THEN
-    CALL fs_read_field(serializer, savepoint, fieldname, field)
->>>>>>> branch 'ftg-zerosize' of github-chovyy:chovyy/serialbox2.git
   END IF
 END SUBROUTINE ftg_read_bool_2d
 
